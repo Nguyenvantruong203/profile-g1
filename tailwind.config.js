69da628b8a2a9c086f10c832433e481b8c65788c
Nguyentruong <truongvjp195@gmail.com>
2023-09-18 20:56:10 +0700

    feat(app): add profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors:{
+        'color-text': '#00C893',
+        'bg-from': '#202324',
+        'w-from': '22%',
+      }
+    },
   },
   plugins: [],
 }
